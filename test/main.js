bef137ed4078cf132901155db50cff45a34a8b35
Scary <camscott@fullsail.edu>
2017-03-20 18:14:33 -0500

    remove version tests

--- test/main.js
+++ test/main.js
@@ -11,30 +11,4 @@ describe('Logging Tests', () => {
   it('should log an error request', () => {
     logger.error('This is an error log');
   });
-
-  const version = require('../lib/main').updateVersion;
-  describe('Lee FS Utility Debugger Test', () => {
-    describe('Test version change function', () => {
-      it('Update major version', () => {
-        const newVersion = version('1.3.2', 'major');
-        expect(newVersion).to.equal('2.0.0');
-      });
-      it('Update minor version', () => {
-        const newVersion = version('1.3.2', 'minor');
-        expect(newVersion).to.equal('1.4.0');
-      });
-      it('Update patch version', () => {
-        const newVersion = version('1.3.2', 'patch');
-        expect(newVersion).to.equal('1.3.3');
-      });
-      it('Bad release entered', () => {
-        const newVersion = version('1.3.2', 'match');
-        expect(newVersion).to.equal('Invalid release');
-      });
-      it('Bad version entered', () => {
-        const newVersion = version('1.3.2.3', 'match');
-        expect(newVersion).to.equal('Invalid version');
-      });
-    });
-  });
 });
