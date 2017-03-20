6e2eaf0eeac6bb849015c261e8a3dd9ea2f26526
Scary <camscott@fullsail.edu>
2017-03-20 17:51:43 -0500

    Version Increaser
    adds the version increaser and unit tests to go with it

--- test/main.js
+++ test/main.js
@@ -1,4 +1,5 @@
 const logger = require('../lib/main.js');
+const expect = require('chai').expect;
 
 // Testing that logs are output
 describe('Logging Tests', () => {
@@ -9,5 +10,31 @@ describe('Logging Tests', () => {
   // Test for a single app
   it('should log an error request', () => {
     logger.error('This is an error log');
+  });
+
+  const version = require('../lib/main').updateVersion;
+  describe('Lee FS Utility Debugger Test', () => {
+    describe('Test version change function', () => {
+      it('Update major version', () => {
+        const newVersion = version('1.3.2', 'major');
+        expect(newVersion).to.equal('2.0.0');
+      });
+      it('Update minor version', () => {
+        const newVersion = version('1.3.2', 'minor');
+        expect(newVersion).to.equal('1.4.0');
+      });
+      it('Update patch version', () => {
+        const newVersion = version('1.3.2', 'patch');
+        expect(newVersion).to.equal('1.3.3');
+      });
+      it('Bad release entered', () => {
+        const newVersion = version('1.3.2', 'match');
+        expect(newVersion).to.equal('Invalid release');
+      });
+      it('Bad version entered', () => {
+        const newVersion = version('1.3.2.3', 'match');
+        expect(newVersion).to.equal('Invalid version');
+      });
+    });
   });
 });
